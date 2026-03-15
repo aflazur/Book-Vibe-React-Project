1abdb309ee790be357f677a39b90ce566a250844
aflazur <mdaflazurrahaman2022@gmail.com>
2026-03-15 23:37:04 +0600

    Environment setup for project

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello World </div>,
+  },
+]);
